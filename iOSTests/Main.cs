09290cebe45b6d3f09a88b580e5172b42cd2f93f
SamDuncanTrend <sam.duncan@honeywell.com>
2016-03-21 15:22:00 +0100

    Unit runner tests[2]

--- iOSTests/Main.cs
+++ iOSTests/Main.cs
@@ -1,7 +1,6 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Collections.Generic;
-
 using Foundation;
 using UIKit;
 
@@ -12,6 +11,7 @@ namespace iOSTests
 		// This is the main entry point of the application.
 		static void Main (string[] args)
 		{
+			Xamarin.Insights.Initialize (global::iOSTests.XamarinInsights.ApiKey);
 			// if you want to use a different Application Delegate class from "UnitTestAppDelegate"
 			// you can specify it here.
 			UIApplication.Main (args, null, "UnitTestAppDelegate");
